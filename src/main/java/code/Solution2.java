733327892bbaf532a2a46e3bcee5d628b5ff1b0e
onceicy <1803105512@qq.com>
2020-07-24 22:56:05 +0800

    solution2

---
+++ src/main/java/code/Solution2.java
@@ -0,0 +1,36 @@
+package code;
+import java.util.*;
+public class Solution2 {
+    public boolean isValid(String s) {
+        Stack<Character> temp = new Stack<Character>();
+        temp.push('#');
+        for (int number = 0; number < s.length(); number++) {
+            switch (s.charAt(number)) {
+                case ')' :
+                    if (temp.peek() != '(') {
+                        return false;
+                    }
+                    temp.pop();
+                    break;
+                case ']' :
+                    if (temp.peek() != '[') {
+                        return false;
+                    }
+                    temp.pop();
+                    break;
+                case '}' :
+                    if (temp.peek() != '{') {
+                        return false;
+                    }
+                    temp.pop();
+                    break;
+                default :
+                    temp.push(s.charAt(number));                       
+            }
+        }  
+        if (temp.peek() != '#') {
+            return false;
+        }
+        return true;
+    }
+}
