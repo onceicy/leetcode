818429d17e42aac372f7d7f9914b61e0f597b13c
onceicy <1803105512@qq.com>
2020-07-25 21:16:10 +0800

    solution 21

---
+++ src/main/java/code/Solution21.java
@@ -0,0 +1,30 @@
+package code;
+
+public class Solution21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }        
+        ListNode first = new ListNode(0);
+        ListNode temp = first;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                temp.next = l2;
+                l2 = l2.next;
+            } else {
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            temp = temp.next;
+        }
+        if (l1 == null) {
+            temp.next = l2;
+        } else {
+            temp.next = l1;
+        }
+        return first.next;
+    }
+}
